da0a2ce2d0c0047a13798eb4d0746863de4fa987
jungwon <mac20010@naver.com>
2023-06-17 18:17:36 +0900

    feat: header 정보.

--- src/main/java/com/example/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/com/example/servlet/basic/request/RequestHeaderServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Enumeration;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,6 +17,7 @@ public class RequestHeaderServlet extends HttpServlet {
             throws ServletException, IOException {
         printStartLine(request);
         printHeaders(request);
+        printHeaderUtils(request);
     }
 
     //start line 정보
@@ -36,15 +38,50 @@ public class RequestHeaderServlet extends HttpServlet {
         System.out.println();
     }
 
+    private void printHeaderUtils(HttpServletRequest request) {
+        System.out.println("--- Header 편의 조회 start ---");
+        System.out.println("[Host 편의 조회]");
+        System.out.println("request.getServerName() = " +
+                request.getServerName()); //Host 헤더
+        System.out.println("request.getServerPort() = " +
+                request.getServerPort()); //Host 헤더
+        System.out.println();
+        System.out.println("[Accept-Language 편의 조회]");
+        request.getLocales().asIterator()
+                .forEachRemaining(locale -> System.out.println("locale = " +
+                        locale));
+        System.out.println("request.getLocale() = " + request.getLocale());
+        System.out.println();
+        System.out.println("[cookie 편의 조회]");
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                System.out.println(cookie.getName() + ": " + cookie.getValue());
+            }
+        }
+        System.out.println();
+        System.out.println("[Content 편의 조회]");
+        System.out.println("request.getContentType() = " +
+                request.getContentType());
+        System.out.println("request.getContentLength() = " +
+                request.getContentLength());
+        System.out.println("request.getCharacterEncoding() = " +
+                request.getCharacterEncoding());
+        System.out.println("--- Header 편의 조회 end ---");
+        System.out.println();
+    }
+
     private void printHeaders(HttpServletRequest request){
         System.out.println("--- Headers - start");
 
-        Enumeration<String> headerNames = request.getHeaderNames();
+//        Enumeration<String> headerNames = request.getHeaderNames();
 
-        while(headerNames.hasMoreElements()){
-            String headerName = headerNames.nextElement();
-            System.out.println(headerName + " : " +headerName);
-        }
+//        while(headerNames.hasMoreElements()){
+//            String headerName = headerNames.nextElement();
+//            System.out.println(headerName + " : " +headerName);
+//        }
+
+        request.getHeaderNames().asIterator()
+                        .forEachRemaining(headerName -> System.out.println(headerName + ":" + headerName));
 
 
         System.out.println("--- Headers - end ---");
