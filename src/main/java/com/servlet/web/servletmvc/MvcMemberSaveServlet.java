bcb48dd9623ce92b28ebe2fd0dc417ce6cc77f99
jungwon <mac20010@naver.com>
2023-07-15 19:24:20 +0900

    feat: 회원 mvc 패턴 적용

--- src/main/java/com/servlet/web/servletmvc/MvcMemberSaveServlet.java
+++ src/main/java/com/servlet/web/servletmvc/MvcMemberSaveServlet.java
@@ -1,34 +1,34 @@
-package com.servlet.web.servlet;
+package com.servlet.web.servletmvc;
 
 import com.servlet.domain.member.Member;
 import com.servlet.domain.member.MemberRepository;
-import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-@WebServlet(name = "memberSaveServlet",urlPatterns = "/servlet/members/save")
-public class MemberSaveServlet extends HttpServlet {
+@WebServlet(name = "mvcMemberSaveServlet", urlPatterns = "/servlet-mvc/members/save")
+public class MvcMemberSaveServlet extends HttpServlet {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         String username = request.getParameter("username");
         int age = Integer.parseInt(request.getParameter("age"));
 
         Member member = new Member(username, age);
         memberRepository.save(member);
 
-        request.setAttribute("member",member);
+        //Model에 데이터를 보관한다.
+        request.setAttribute("member", member);
 
         String viewPath = "/WEB-INF/views/save-result.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
-        dispatcher.forward(request,response);
+        dispatcher.forward(request, response);
     }
 }
